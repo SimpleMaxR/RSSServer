4f9ead4e65856ec9743c3f6cb554d462b3fda180
SimpleMaxR <hugoho912@outlook.com>
2023-09-04 19:43:16 +0800

    now able to create users and get api keys

---
+++ internal/auth/auth.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
+
+func GetAPIKey(header http.Header) (string, error) {
+	authHeader := header.Get("Authorization")
+	if authHeader == "" {
+		return "", ErrNoAuthHeaderIncluded
+	}
+	splitAuth := strings.Split(authHeader, " ")
+	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
+		return "", errors.New("malformed auth header")
+	}
+
+	return splitAuth[1], nil
+}
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"RSSServer/internal/database"
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
+func main() {
+	// 配置环境
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	dbURL := os.Getenv("DB_URL")
+
+	// 设定与 database 链接
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("sql.Open fail")
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
+
+	// 创建路由
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	// 创建二级路由
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+
+	// 配置二级路由
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
